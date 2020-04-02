4ddcb6626c76b20cd718674a551ca1fa29d6e247
canhlx.299 <canhlx.299@MacBook-Pro-cua-canhlx299.local>
2020-04-02 21:55:40 +0700

    canhlx

--- todo-mvc/src/app/app.module.ts
+++ todo-mvc/src/app/app.module.ts
@@ -1,16 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { TodoListComponent } from "./components/todo-list/todo-list.component";
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
+import { TodoInputComponent } from './components/todo-input/todo-input.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, TodoListComponent, TodoItemComponent, TodoInputComponent, HeaderComponent, FooterComponent],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
